565f541b9dce54f76f9a2029a18d799303573b3f
Cheeshcake <mohamedazizhadjhassen@gmail.com>
2024-07-03 00:39:37 +0100

    delivery services section

---
+++ src/js/services.js
@@ -0,0 +1,17 @@
+export const services = [
+  {
+    image: "/assets/choose-coffee.png",
+    title: "choose your coffee",
+    description:"there are 20+ coffees for you",
+  },
+  {
+    image: "/assets/food-truck.png",
+    title: "we delivery it to you",
+    description:"Choose delivery service",
+  },
+  {
+    image: "/assets/coffee-cup.png",
+    title: "Enjoy your coffee",
+    description:"Choose delivery service",
+  },
+]
